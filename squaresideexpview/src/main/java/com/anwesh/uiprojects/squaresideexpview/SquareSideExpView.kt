f7169840ae6ac4f1e070ed92340f6d0d8b3e3607
Anwesh43 <anweshthecool0@gmail.com>
2019-03-25 09:13:03 +0530

    adding SSENode class

--- squaresideexpview/src/main/java/com/anwesh/uiprojects/squaresideexpview/SquareSideExpView.kt
+++ squaresideexpview/src/main/java/com/anwesh/uiprojects/squaresideexpview/SquareSideExpView.kt
@@ -125,4 +125,48 @@ class SquareSideExpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SSENode(var i : Int, val state : State = State()) {
+
+        private var prev : SSENode? = null
+        private var next : SSENode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = SSENode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSSENode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SSENode {
+            var curr : SSENode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
