b716724f2b8e60e97bb444121d21bdecb22760ef
Anwesh43 <anweshthecool0@gmail.com>
2019-03-25 09:24:33 +0530

    adding static create method

--- squaresideexpview/src/main/java/com/anwesh/uiprojects/squaresideexpview/SquareSideExpView.kt
+++ squaresideexpview/src/main/java/com/anwesh/uiprojects/squaresideexpview/SquareSideExpView.kt
@@ -216,4 +216,13 @@ class SquareSideExpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : SquareSideExpView {
+            var view : SquareSideExpView = SquareSideExpView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
