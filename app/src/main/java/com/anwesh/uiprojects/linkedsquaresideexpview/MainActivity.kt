270c47f064c54a2412460ae1b84bf0311447a36a
Anwesh43 <anweshthecool0@gmail.com>
2019-03-25 09:34:32 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedsquaresideexpview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedsquaresideexpview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedsquaresideexpview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.squaresideexpview.SquareSideExpView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SquareSideExpView.create(this)
     }
 }
