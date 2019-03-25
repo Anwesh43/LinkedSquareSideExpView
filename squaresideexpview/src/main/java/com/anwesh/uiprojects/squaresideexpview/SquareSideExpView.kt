8da87b456f6c731295b64c388c3ee0199fe5c408
Anwesh43 <anweshthecool0@gmail.com>
2019-03-25 09:05:46 +0530

    adding drawSSENode canvas extension method

--- squaresideexpview/src/main/java/com/anwesh/uiprojects/squaresideexpview/SquareSideExpView.kt
+++ squaresideexpview/src/main/java/com/anwesh/uiprojects/squaresideexpview/SquareSideExpView.kt
@@ -11,6 +11,7 @@ import android.content.Context
 import android.graphics.Paint
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Path
 
 val nodes : Int = 5
 val squares : Int = 4
@@ -26,4 +27,36 @@ fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
-fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap 
+fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+fun Int.sjf() : Float = 1f - (this % 2)
+
+fun Canvas.drawSSENode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    val yGap : Float = size / squares
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.color = foreColor
+    paint.style = Paint.Style.STROKE
+    save()
+    translate(w / 2, gap * (i + 1))
+    for (j in 0..(squares - 1)) {
+        val sc : Float = sc1.divideScale(j, squares)
+        val x : Float = yGap * sc * j.sjf()
+        save()
+        translate(x, yGap * j)
+        val path : Path = Path()
+        path.moveTo(0f, 0f)
+        path.lineTo(x, 0f)
+        path.lineTo(x, yGap)
+        path.lineTo(0f, yGap)
+        path.lineTo(0f, 0f)
+        drawPath(path, paint)
+        restore()
+    }
+    restore()
+}
